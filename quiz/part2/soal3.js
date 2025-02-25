3fee0c3735acb8e17491a0f19b62f57a23df7d9f
mzackyaufaaa <mzackyaufa@gmail.com>
2025-02-25 15:58:51 +0700

    part2 soal3 dan soal4

---
+++ quiz/part2/soal3.js
@@ -0,0 +1,13 @@
+let word3 = 'wow JavaScript is so cool';
+
+const FirstWord3 = word3.substring(0, 3);
+const SecondWord3 = word3.substring(4, 14);
+const ThirdWord3 = word3.substring(15, 17);
+const FourthWord3 = word3.substring(18, 20);
+const FifthWord3 = word3.substring(21, 25);
+
+console.log('First Word: ' + FirstWord3);
+console.log('Second Word: ' + SecondWord3);
+console.log('Third Word: ' + ThirdWord3);
+console.log('Fourth Word: ' + FourthWord3);
+console.log('Fifth Word: ' + FifthWord3);
---
+++ quiz/part2/soal4.js
@@ -0,0 +1,19 @@
+let word4 = 'wow JavaScript is so cool';
+
+let pertama = word4.substring(0, 3);
+let kedua = word4.substring(4, 14);
+let ketiga = word4.substring(15, 17);
+let keempat = word4.substring(18, 20);
+let kelima = word4.substring(21, 25);
+
+let pertamaLength = pertama.length;
+let keduaLength = kedua.length;
+let ketigaLenght = ketiga.length;
+let keempatLength = keempat.length;
+let kelimaLength = kelima.length;
+
+console.log('First Word: ' + pertama + ', with length: ' + pertamaLength);
+console.log('First Word: ' + kedua + ', with length: ' + keduaLength);
+console.log('First Word: ' + ketiga + ', with length: ' + ketigaLenght);
+console.log('First Word: ' + keempat + ', with length: ' + keempatLength);
+console.log('First Word: ' + kelima + ', with length: ' + kelimaLength);
